48509af27823aa3ed27fdecba97ef1a074810bc3
Makerspace92 <100864900+Makerspace92@users.noreply.github.com>
2023-07-25 13:16:15 -0400

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,7 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        System.Console.WriteLine(moves.Length);
         return moves[0];
     }
 }
